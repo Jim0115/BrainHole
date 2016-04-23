5ab5cd71b93ddcb4a0666364e99cdb21e69c625e
Jim0115 <Jim0115@users.noreply.github.com>
2016-04-23 19:21:40 +0800

    Finish view by code.

--- SetConstraintsByCode/SetConstraintsByCode/ViewController.swift
+++ SetConstraintsByCode/SetConstraintsByCode/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  var colorfulViews = [UIView]()
+  
+  let standardSpacingBetweenViews: CGFloat = 8.0
+  let standardSpacingWithSuperView: CGFloat = 20
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    createViews()
+    
+    for v in colorfulViews {
+      view.addSubview(v)
+    }
+    
+    addConstraints()
+    
+    title = "Autolayout by code"
+  
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func createViews() {
+    let redView = UIView()
+    redView.backgroundColor = UIColor.redColor()
+    redView.translatesAutoresizingMaskIntoConstraints = false
+    colorfulViews.append(redView)
+    let greenView = UIView()
+    greenView.backgroundColor = UIColor.greenColor()
+    greenView.translatesAutoresizingMaskIntoConstraints = false
+    colorfulViews.append(greenView)
+    let yellowView = UIView()
+    yellowView.backgroundColor = UIColor.yellowColor()
+    yellowView.translatesAutoresizingMaskIntoConstraints = false
+    colorfulViews.append(yellowView)
+    let blueView = UIView()
+    blueView.backgroundColor = UIColor.blueColor()
+    blueView.translatesAutoresizingMaskIntoConstraints = false
+    colorfulViews.append(blueView)
   }
-
-
+  
+  func addConstraints() {
+    // aspect ratio
+    for v in colorfulViews {
+      v.addConstraint(NSLayoutConstraint(item: v, attribute: .Width, relatedBy: .Equal, toItem: v, attribute: .Height, multiplier: 1, constant: 0))
+    // vertically center
+      view.addConstraint(NSLayoutConstraint(item: v, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1, constant: 0))
+    }
+    
+    for i in 1...3 {
+      // spacing
+      view.addConstraint(NSLayoutConstraint(item: colorfulViews[i], attribute: .Leading, relatedBy: .Equal, toItem: colorfulViews[i - 1], attribute: .Trailing, multiplier: 1, constant: standardSpacingBetweenViews))
+      // equal width
+      view.addConstraint(NSLayoutConstraint(item: colorfulViews[i], attribute: .Width, relatedBy: .Equal, toItem: colorfulViews[i - 1], attribute: .Width, multiplier: 1, constant: 0))
+    }
+    // first and last
+    view.addConstraint(NSLayoutConstraint(item: colorfulViews[0], attribute: .Left, relatedBy: .Equal, toItem: view, attribute: .Left, multiplier: 1, constant: standardSpacingWithSuperView))
+    view.addConstraint(NSLayoutConstraint(item: colorfulViews[3], attribute: .Right, relatedBy: .Equal, toItem: view, attribute: .Right, multiplier: 1, constant: -standardSpacingWithSuperView))
+  }
+  
 }
 
