4e528a598756ebf4be26c9efb43c581caf5a453e
Jim0115 <Jim0115@users.noreply.github.com>
2016-05-20 09:37:25 +0800

    Create Contact entity.

---
+++ Contacts/Contacts/Contact+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  Contact+CoreDataProperties.swift
+//  Contacts
+//
+//  Created by 王仕杰 on 5/20/16.
+//  Copyright © 2016 王仕杰. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Contact {
+
+    @NSManaged var firstName: String?
+    @NSManaged var lastName: String?
+    @NSManaged var phone: String?
+
+}
---
+++ Contacts/Contacts/Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by 王仕杰 on 5/20/16.
+//  Copyright © 2016 王仕杰. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Contact: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
