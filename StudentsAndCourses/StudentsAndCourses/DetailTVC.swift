42c210a6b9d308679486e97c33f07ddd4588ab10
Jim0115 <Jim0115@users.noreply.github.com>
2016-05-24 09:27:28 +0800

    Prepare for database helper.

--- StudentsAndCourses/StudentsAndCourses/DetailTVC.swift
+++ StudentsAndCourses/StudentsAndCourses/DetailTVC.swift
@@ -13,15 +13,6 @@ class DetailTVC: UITableViewController {
   var texts = [String]()
   var details = [String]()
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-  }
-  
   // MARK: - Table view data source
   
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,26 +30,26 @@ class DetailTVC: UITableViewController {
     return cell
   }
   
+  // MARK: - table view delegate
   
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
   
-  /*
-   // Override to support editing the table view.
-   override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-   if editingStyle == .Delete {
-   // Delete the row from the data source
-   tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-   } else if editingStyle == .Insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
+  // Override to support conditional editing of the table view.
+  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+  }
+  
+  
+  
+  // Override to support editing the table view.
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+      texts.removeAtIndex(indexPath.row)
+      details.removeAtIndex(indexPath.row)
+      tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    }
+  }
+  
   
   /*
    // Override to support rearranging the table view.
