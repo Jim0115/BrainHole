9843281bd18dcd707c90fe8cf59f154b0ef710ac
Jim0115 <Jim0115@users.noreply.github.com>
2016-06-06 10:50:46 +0800

    "Jun 06, 2016 10:50, 158"

---
+++ TicTacToe/TicTacToe/GameAI.swift
@@ -0,0 +1,13 @@
+//
+//  GameAI.swift
+//  TicTacToe
+//
+//  Created by 王仕杰 on 6/4/16.
+//  Copyright © 2016 王仕杰. All rights reserved.
+//
+
+import Foundation
+
+class GameAI {
+  var condition = [(Signal, Int, Int)]()
+}
