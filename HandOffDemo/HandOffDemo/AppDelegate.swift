db472f8f4bd743452a41a47706f0aea1db74f32c
Jim0115 <Jim0115@users.noreply.github.com>
2016-09-05 21:53:20 +0800

    Implement user activity method.

--- HandOffDemo/HandOffDemo/AppDelegate.swift
+++ HandOffDemo/HandOffDemo/AppDelegate.swift
@@ -15,32 +15,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    // Override point for customization after application launch.
+    
+//    if ContactHelper.sharedInstance.allContact.count == 1 {
+//      ContactHelper.sharedInstance.insertContact(email: "testEmail")
+//    }
+    
+//    ContactHelper.sharedInstance.clearAllContact()
+    
+    print("Current Contacts:")
+    print(ContactHelper.sharedInstance.allContacts)
+    
     return true
   }
-
-  func applicationWillResignActive(application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+  
+  func application(application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
+    print("userActivityType " + userActivityType)
+    return false
   }
-
-  func applicationDidEnterBackground(application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+  
+  func application(application: UIApplication, continueUserActivity userActivity: NSUserActivity, restorationHandler: ([AnyObject]?) -> Void) -> Bool {
+//    if let window = window {
+      let navigationVC = window?.rootViewController as? UINavigationController
+      let viewController = navigationVC?.topViewController as? ContactsViewController
+      
+      if viewController?.presentedViewController == nil {
+        let newNavi = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()).instantiateViewControllerWithIdentifier("newContactNavi") as! UINavigationController
+        viewController?.presentViewController(newNavi, animated: true, completion: nil)
+        
+        restorationHandler([newNavi.topViewController!])
+      } else {
+        if let newContactVC = (viewController?.presentedViewController as? UINavigationController)?.topViewController as? NewContactViewController {
+          restorationHandler([newContactVC])
+        }
+      }
+      
+      
+      
+      return true
+//    } else {
+//      return false
+//    }
+    
+    
   }
-
-  func applicationWillEnterForeground(application: UIApplication) {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-  }
-
-  func applicationDidBecomeActive(application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-  }
-
-  func applicationWillTerminate(application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-  }
-
-
 }
 
