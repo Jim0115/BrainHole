d57e259f74a1f22e6eac72925b92ab86fe162f5c
Jim0115 <Jim0115@users.noreply.github.com>
2016-09-06 17:50:55 +0800

    Implement method for user activity.

--- HandOff-iOS-macOS/HandOff-iOS-macOS/ViewController.swift
+++ HandOff-iOS-macOS/HandOff-iOS-macOS/ViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  @IBOutlet weak var addressField: UITextField!
+  @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    userActivity = NSUserActivity(activityType: "cn.edu.neu.wangshijie.HandOff-iOS-macOS.updateURL")
+    userActivity?.becomeCurrent()
+    
+    indicatorView.hidesWhenStopped = true
+    indicatorView.stopAnimating()
   }
   
+  @IBAction func goAction(sender: UIButton) {
+    if let text = addressField.text {
+      if let persentedText = text.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet()) {
+        userActivity?.webpageURL = NSURL(string: "https://www.baidu.com/s?wd=" + persentedText)
+      }
+    }
+    
+    sender.hidden = true
+    indicatorView.startAnimating()
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(NSEC_PER_SEC * 5)), dispatch_get_main_queue()) {
+      self.indicatorView.stopAnimating()
+      sender.hidden = false
+    }
+  }
+  
+
 }
 
