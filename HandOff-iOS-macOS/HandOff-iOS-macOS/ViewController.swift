033afcbcee7154388d5c08c1f741060160b29e08
Jim0115 <Jim0115@users.noreply.github.com>
2016-09-06 14:05:02 +0800

    Initial commit.

---
+++ HandOff-iOS-macOS/HandOff-iOS-macOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HandOff-iOS-macOS
+//
+//  Created by 王仕杰 on 9/6/16.
+//  Copyright © 2016 王仕杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+}
+
