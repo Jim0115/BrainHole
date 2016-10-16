1623b56157f11d6a33e16a4cb34bf7e538624ef4
Jim0115 <Jim0115@users.noreply.github.com>
2016-10-16 22:22:04 +0800

    Initial commit.

---
+++ Hypnosister/Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by 王仕杰 on 16/10/2016.
+//  Copyright © 2016 王仕杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by 王仕杰 on 16/10/2016.
+//  Copyright © 2016 王仕杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
