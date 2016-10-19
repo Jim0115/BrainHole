666ed9b20386a73dfba7d6a1f85b1b14ad5a0930
Jim0115 <Jim0115@users.noreply.github.com>
2016-10-19 23:25:42 +0800

    Core Graphics.

--- Hypnosister/HypnosisterView.m
+++ Hypnosister/HypnosisterView.m
@@ -19,43 +19,52 @@
 }
 
 - (void)drawRect:(CGRect)rect {
-//  CGFloat radius = MIN(self.bounds.size.width, self.bounds.size.height) / 2;
-  CGFloat maxRadius = hypotf(self.bounds.size.width, self.bounds.size.height) / 2;
   
-  CGContextRef currentContext = UIGraphicsGetCurrentContext();
+    CGFloat maxRadius = hypotf(self.bounds.size.width, self.bounds.size.height) / 2;
   
-  CGContextSetRGBStrokeColor(currentContext, 1, 1, 1, 1);
+  
+  CGContextRef contextRef = UIGraphicsGetCurrentContext();
+  
+  CGFloat components[] = {1, 0, 0, 1};
+  CGColorRef color = CGColorCreate(CGColorSpaceCreateDeviceRGB(), components);
+  CGContextSetStrokeColorWithColor(contextRef, color);
+  
+//  CGContextMoveToPoint(contextRef, 100, 100);
+//  CGContextAddLineToPoint(contextRef, 200, 200);
   
   CGMutablePathRef path = CGPathCreateMutable();
   
+  
   for (CGFloat currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
     CGPathMoveToPoint(path, NULL, self.center.x + currentRadius, self.center.y);
-    CGPathAddArc(path, NULL, self.center.x, self.center.y, currentRadius, 0, M_PI * 2, true);
+    CGPathAddArc(path, NULL, self.center.x, self.center.y, currentRadius, 0, M_PI * 2, false);
   }
   
-  CGContextAddPath(currentContext, path);
-  CGContextStrokePath(currentContext);
+  CGContextAddPath(contextRef, path);
+  
+  CGContextSetLineWidth(contextRef, 5);
+  
+  CGContextStrokePath(contextRef);
+  
   CGPathRelease(path);
   
-  CGContextSetStrokeColorWithColor(currentContext, [[UIColor redColor] CGColor]);
-  
-//  UIBezierPath* path = [UIBezierPath new];
-//
-//  for (CGFloat currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
-//    [path moveToPoint:CGPointMake(self.center.x + currentRadius, self.center.y)];
-//    
-//    [path addArcWithCenter:self.center
-//                    radius:currentRadius
-//                startAngle:0
-//                  endAngle:M_PI * 2
-//                 clockwise:YES];
-//  }
-//  
-//  path.lineWidth = 10;
-//  
-//  [[UIColor lightGrayColor] setStroke];
-//
-//  [path stroke];
+  //  UIBezierPath* path = [UIBezierPath new];
+  //
+  //  for (CGFloat currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+  //    [path moveToPoint:CGPointMake(self.center.x + currentRadius, self.center.y)];
+  //
+  //    [path addArcWithCenter:self.center
+  //                    radius:currentRadius
+  //                startAngle:0
+  //                  endAngle:M_PI * 2
+  //                 clockwise:YES];
+  //  }
+  //
+  //  path.lineWidth = 10;
+  //
+  //  [[UIColor lightGrayColor] setStroke];
+  //
+  //  [path stroke];
 }
 
 
