0a581cffc5f8dddd83d3e1b37cbb4a453d7e00a5
Jim0115 <Jim0115@users.noreply.github.com>
2016-10-30 17:52:48 +0800

    Initial commit.

---
+++ Homepwner/Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by 王仕杰 on 2016/10/30.
+//  Copyright © 2016年 王仕杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/Homepwner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homepwner
+//
+//  Created by 王仕杰 on 2016/10/30.
+//  Copyright © 2016年 王仕杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
