3ca5f81eeabd27c7b7bc6a0d0b377eebe693efef
Jim0115 <Jim0115@users.noreply.github.com>
2016-11-06 23:53:54 +0800

    Nov 06, 2016 23:53, 311

--- Homepwner/Homepwner/BNRItemsViewController.m
+++ Homepwner/Homepwner/BNRItemsViewController.m
@@ -11,6 +11,12 @@
 #import "BNRItemStore.h"
 #import "BNRItem.h"
 
+@interface BNRItemsViewController ()
+
+@property (nonatomic, strong) IBOutlet UIView* headerView;
+
+@end
+
 @implementation BNRItemsViewController
 
 - (instancetype)init {
@@ -27,12 +33,44 @@
   return self;
 }
 
+- (UIView *)headerView {
+  if (!_headerView) {
+    [[NSBundle mainBundle] loadNibNamed:@"HeaderView"
+                                  owner:self
+                                options:nil];
+  }
+  return _headerView;
+}
+
 - (void)viewDidLoad {
   [super viewDidLoad];
   
   [self.tableView registerClass:[UITableViewCell class]
-        forCellReuseIdentifier:@"UITableViewCell"];
+         forCellReuseIdentifier:@"UITableViewCell"];
+  
+  self.tableView.tableHeaderView = self.headerView;
 }
+
+#pragma mark - Actions
+
+- (IBAction)toggleEditingMode:(UIButton *)sender {
+  if (self.editing) {
+    [sender setTitle:@"Edit"
+            forState:UIControlStateNormal];
+    [self setEditing:NO
+            animated:YES];
+  } else {
+    [sender setTitle:@"Done"
+            forState:UIControlStateNormal];
+    [self setEditing:YES
+            animated:YES];
+  }
+}
+
+- (IBAction)addNewItem:(UIButton *)sender {
+  
+}
+
 
 #pragma mark - tableViewDataSource
 
@@ -41,8 +79,8 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-//  UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
-//                                                 reuseIdentifier:@"UITableViewCell"];
+  //  UITableViewCell* cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+  //                                                 reuseIdentifier:@"UITableViewCell"];
   UITableViewCell* cell = [self.tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
                                                                forIndexPath:indexPath];
   
